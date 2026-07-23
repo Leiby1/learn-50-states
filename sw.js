6f726f83b02321f5b09030498a51ace0cca05193
Leiby1 <81263763+Leiby1@users.noreply.github.com>
2026-07-23 09:00:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Cache name — bump this string any time you want to force-clear old
 // cached files (rarely needed).
-const CACHE_NAME = "states-app-v15";
+const CACHE_NAME = "states-app-v18";
 
 self.addEventListener("install", () => {
   self.skipWaiting();
